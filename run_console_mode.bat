ede8c665cb49d595db2e22195c4a14dd29eb0dd9
kazurayam <kazuaki.urayama@gmail.com>
2018-09-01 06:48:41 +0900

    add docs/red_ball.png

--- run_console_mode.bat
+++ run_console_mode.bat
@@ -6,7 +6,7 @@ set DIR=%~dp0
 set SCRIPT_DIR=%DIR:~0,-1%
 echo SCRIPT_DIR=%SCRIPT_DIR%
 cd "%KATALONSTUDIO_HOME%"
-.\katalon.exe -noSplash -runMode=console -summaryReport -projectPath="%SCRIPT_DIR%" -testSuitePath="Test Suites\TS0" -browserType="Firefox" -headless --config -proxyOption=MANUAL_CONFIG -proxy.server.type=HTTP -proxy.server.address="172.24.2.10" -proxy.server.port="8080"
+.\katalon.exe -noSplash -runMode=console -summaryReport -projectPath="%SCRIPT_DIR%" -testSuitePath="Test Suites\TS0" -browserType="Firefox (headless)" --config -proxyOption=MANUAL_CONFIG -proxy.server.type=HTTP -proxy.server.address="172.24.2.10" -proxy.server.port="8080"
 set exitCode=%ERRORLEVEL%
 cd /d "%SCRIPT_DIR%"
 
